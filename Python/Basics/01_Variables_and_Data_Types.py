a37e3985a5f7b37387f90aeb68f4cfd2e2315c00
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-07-01 09:38:25 +0530

    Complete Mission 1: Variables and Data types

---
+++ Python/Basics/01_Variables_and_Data_Types.py
@@ -0,0 +1,19 @@
+#Variables in pyhton
+
+name = "Swathi"
+date_of_birth = "09-12-2004"
+course = "M.Sc Applied Data Science"
+cgpa = 8.67
+is_student = True
+
+print("Name:", name)
+print("Date of Birth:", date_of_birth)
+print("Course:", course)
+print("CGPA:", cgpa)
+print("Is Student:", is_student)
+
+print(type(name))
+print(type(date_of_birth))
+print(type(course))
+print(type(cgpa))
+print(type(is_student))
---
+++ Python/Basics/about_me.py
@@ -0,0 +1,24 @@
+name = "Swathi"
+age = 22
+date_of_birth = "09-12-2004"
+course = "M.Sc Applied Data Science"
+favorite_programming_language = "Python"
+dream_country = "Any country"
+dream_job = "Data Scientist"
+
+print("Name:", name)
+print("Age:", age)  
+print("Date of Birth:", date_of_birth)
+print("Course:", course)
+print("Favorite Programming Language:", favorite_programming_language)
+print("Dream Country:", dream_country)
+print("Dream Job:", dream_job)
+
+print(type(name))
+print(type(age))
+print(type(date_of_birth))
+print(type(course))
+print(type(favorite_programming_language))
+print(type(dream_country))
+print(type(dream_job))
+
