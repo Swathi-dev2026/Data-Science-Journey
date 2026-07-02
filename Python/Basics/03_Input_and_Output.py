a3da25f58b5cda90b9c31d20e4934ed8dd9c0388
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-07-02 21:37:19 +0530

    Complete Mission 3: Python Inputs and Outputs

---
+++ Python/Basics/03_Input_and_Output.py
@@ -0,0 +1,31 @@
+#name = input("Enter your name: ")
+#age = input ("Enter your age: ")
+#course = input ("Enter your course name: ")
+#print()
+#print("Name : ", name)
+#print("Age : ", age)
+#print("Course : ", course)
+
+name = input ("Enter your name: ")
+age = input ("Enter your age: ")
+Favorite_programming_language = input ("Enter your favorite programming language: ")
+
+print("==================PROFILE=================")
+print("Name : ",name)
+print("Age : ",age)
+print("Favorite programming language : ",Favorite_programming_language)
+print("===========================================")
+
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
+addition = first_number + second_number
+print("Addition of two numbers is: ", addition)
+subtraction = first_number - second_number
+print("Subtraction of two numbers is: ", subtraction)
+multiplication = first_number * second_number
+print("Multiplication of two numbers is: ", multiplication)
+division = first_number / second_number
+print("Division of two numbers is: ", division)
+
+
+
