ff034fb5d213cb9de37ad2300d056e20cc3628c5
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-07-03 08:10:43 +0530

    Add Mini Project 1: Personal Profile Card

---
+++ Projects/01_Personal_Profile_Card/README.md
@@ -0,0 +1,17 @@
+# Personal Profile Card
+
+## Description
+This is my first Python mini project.
+
+The program asks the user for personal details and displays them in a formatted profile card.
+
+## Concepts Used
+- Variables
+- Input & Output
+- Data Types
+- print()
+
+## Author
+Swathi
+
+## Output Screenshot ![Program Output](screenshots/output.png)
---
+++ Projects/01_Personal_Profile_Card/main.py
@@ -0,0 +1,29 @@
+
+
+name = input("Enter your name : " )
+age = int(input("Enter your age : "))
+degree = input("Enter your degree : ")
+university = input("Enter your university : ")
+favorite_programming_language = input("Enter your favorite programming language : ")
+dream_job = input("Enter your dream job : ")
+dream_country = input("Enter your dream country : ")
+how_many_hours_do_you_study_per_day = int(input("How many hours do you study per day : "))
+
+print ("=====================================================")
+print ("================PERSONAL PROFILE CARD================")
+print ("=====================================================")
+print ("Name : ", name)
+print ("Age : ", age)
+print ("Degree : ", degree)
+print ("University : ", university)
+print ("Favorite Programming Language : ", favorite_programming_language)
+print ("Dream Job : ", dream_job)
+print ("Dream Country : ", dream_country)
+print ("Hours Studied Per Day : ", how_many_hours_do_you_study_per_day) 
+print ("======================================================")
+print ("============THANK YOU FOR USING MY PROGRAM============")
+print ("======================================================")
+
+print ("Keep going, ", name, "!")
+print ("You are doing great, ", name, "!")
+
