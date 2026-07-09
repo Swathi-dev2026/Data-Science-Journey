4fdd65f0dd195ba9bf9847e020e77b1f10b6b5cc
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-07-09 10:19:08 +0530

    Complete Mission 7: List

--- Python/Basics/07_List.py
+++ Python/Basics/07_List.py
@@ -29,3 +29,39 @@ print("="*50)
 # Loop through a list
 for fruit in fruits:
     print(fruit)
+print("="*50)
+
+# Challenge 1
+print("Challenges")
+programming_languages = ["python","java","SQL","numpy","pandas"]
+print(programming_languages)
+print("="*50)
+
+# Challenge 2
+print (programming_languages[3])
+print("="*50)
+
+# challenge 3
+programming_languages[1] = "scikit"
+print(programming_languages)
+print("="*50)
+
+# challenge 4
+programming_languages.append("javascript")
+print(programming_languages)
+print("="*50)
+
+# challenge 5
+programming_languages.remove("python")
+print(programming_languages)
+print("="*50)
+
+# challenge 6
+print(len(programming_languages))
+print(programming_languages)
+print("="*50)
+
+# Bonus challenge
+print("Bonus Challenge")
+for language in programming_languages:
+    print(f"I want to learn {language}")
