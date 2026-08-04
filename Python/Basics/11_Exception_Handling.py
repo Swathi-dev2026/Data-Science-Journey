1241da0ce3b8d233d94e9e0d09b36259799866ea
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-08-04 20:02:22 +0530

    started learning exception handling

---
+++ Python/Basics/11_Exception_Handling.py
@@ -0,0 +1,45 @@
+
+try:
+    age = int(input("Enter your age: "))
+    print(f"Your age is {age}")
+except:
+    print("Please enter a valid number.")
+print("="*50)
+
+# Specific Errors
+
+try:
+    age = int(input("Enter your age: "))
+    print(f"Your age is {age}")
+except ValueError:
+    print("Please enter a valid number.")
+print("="*50)
+
+# different types of errors
+
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+    print(f"Result: {result}")
+
+except ValueError:
+    print("Please enter a valid number.")
+
+except ZeroDivisionError:
+    print("You cannot divide by zero.")
+print("="*50)
+
+# else block
+
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+
+except ValueError:
+    print("Please enter a valid number.")
+
+except ZeroDivisionError:
+    print("You cannot divide by zero.")
+
+else:
+    print(f"Result: {result}")
