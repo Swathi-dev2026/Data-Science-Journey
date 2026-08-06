73ab7bb798f244e559beeea3bd4a18d83d972b6b
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-08-06 19:26:09 +0530

    Started learning OOP

---
+++ Python/Basics/13_Object_Oriented_Programming.py
@@ -0,0 +1,39 @@
+
+#class Student:
+ #   pass
+
+#student1 = Student()
+
+#print(student1)
+
+### Adding data to a class
+
+#class Student:
+
+ #   def __init__(self, name, age):
+  #      self.name = name
+   #     self.age = age
+
+#student1 = Student("Swathi", 22)
+
+#print(student1.name)
+#print(student1.age)
+
+### Methods
+
+#class Student:
+
+ #   def __init__(self, name, age):
+  #      self.name = name
+   #     self.age = age
+
+    #def display(self):
+     #   print(f"Name: {self.name}")
+      #  print(f"Age: {self.age}")
+
+#student1 = Student("Swathi", 22)
+
+#student1.display()
+
+### calling methods on multiple objects
+
