3bd2451c6d9e674039c8697463dbd030d82403ea
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-08-06 20:48:07 +0530

    Completed Mission 13: Object Oriented Programming (OOP)

--- Python/Basics/13_Object_Oriented_Programming.py
+++ Python/Basics/13_Object_Oriented_Programming.py
@@ -37,3 +37,41 @@
 
 ### calling methods on multiple objects
 
+#class Student:
+
+ #   def __init__(self, name, age):
+  #      self.name = name
+   #     self.age = age
+
+    #def display(self):
+     #   print(f"Name: {self.name}")
+      #  print(f"Age : {self.age}")
+       # print("--------------------")
+
+#student1 = Student("Swathi", 22)
+#student2 = Student("Rahul", 21)
+#student3 = Student("Anjali", 23)
+
+#student1.display()
+#student2.display()
+#student3.display()
+
+### Adding another methon
+
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(f" Name : {self.name}")
+        print(f" Age : {self.age}")
+
+    def greet(self):
+        print(f"Hello, {self.name}! Welcome to python.")
+
+student1 = Student("Swathi", 22)
+
+student1.display()
+student1.greet()
