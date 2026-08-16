1f5a4042c27f2861b95b9609a9bc3448e5799b0b
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-08-16 16:56:23 +0530

    Completed Mission 14: OPP Student Manager

--- Python/Basics/14_OOP_Student_Manager.py
+++ Python/Basics/14_OOP_Student_Manager.py
@@ -1,27 +1,34 @@
+class Person:
 
-class Student:
-
-    def __init__(self, name, age, course):
+    def __init__(self, name, age):
         self.name = name
         self.__age = age
-        self.course = course
-
-    def display(self):
-        print("===================================")
-        print(f"Name   : {self.name}")
-        print(f"Age    : {self.__age}")
-        print(f"Course : {self.course}")
-        print("===================================")
 
     def introduce(self):
-        print(f"Hello, I am {self.name}.")
+        print(f"Hello, Iam {self.name}.")
+
+    def display_basic_info(self):
+        print(f"Name  : {self.name}")
+        print(f"Age  : {self.__age}")
+
+class Student(Person):
+
+    def __init__(self, name, age, course):
+        super().__init__(name, age)
+        self.course = course
+
+    def display_student_info(self):
+        print("===================================")
+        self.display_basic_info()
+        print(f"Course : {self.course}")
+        print("===================================")
 
 student1 = Student("Swathi", 22, "Data Science")
 student2 = Student("Akash", 21, "Machine Learning")
 
-student1.display()
+student1.display_student_info()
 student1.introduce()
 
-student2.display()
+student2.display_student_info()
 student2.introduce()
 
