358cd2f3fcab0560076a12833717dfcdd56f7c42
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-08-18 14:26:43 +0530

    Modified about me

--- about_me.py
+++ about_me.py
@@ -1,6 +1,5 @@
 name = "Swathi"
 age = 22
-date_of_birth = "09-12-2004"
 course = "M.Sc Applied Data Science"
 favorite_programming_language = "Python"
 dream_country = "Any country"
@@ -8,7 +7,6 @@ dream_job = "Data Scientist"
 
 print("Name:", name)
 print("Age:", age)  
-print("Date of Birth:", date_of_birth)
 print("Course:", course)
 print("Favorite Programming Language:", favorite_programming_language)
 print("Dream Country:", dream_country)
@@ -16,7 +14,6 @@ print("Dream Job:", dream_job)
 
 print(type(name))
 print(type(age))
-print(type(date_of_birth))
 print(type(course))
 print(type(favorite_programming_language))
 print(type(dream_country))
