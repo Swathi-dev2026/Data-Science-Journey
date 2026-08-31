957d8dddc95f86534d2ced17e1c3bcde62e5c1ee
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-08-31 20:18:39 +0530

    Completed Mission 27: Decorators

--- Python/Basics/27_Decorators.py
+++ Python/Basics/27_Decorators.py
@@ -6,10 +6,23 @@ def my_decorator(func):
         print("After the function")
     return wrapper
 
-
 @my_decorator
 def say_hello():
     print("Hello!")
 
-
 say_hello()
+print("="*70)
+### Practical example: Logging
+def log_function(func):
+    def wrapper():
+        print("Function is Starting...")
+        func()
+        print("Function has finished.")
+
+    return wrapper
+
+@log_function
+def calculate():
+    print("Calculating...")
+
+calculate()
