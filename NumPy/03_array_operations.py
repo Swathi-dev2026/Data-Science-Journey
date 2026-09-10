56bd92e58fd703e8bff9d41048a3405c9a971929
Swathi-dev2026 <swathi.dev2026@gmail.com>
2026-09-10 19:55:12 +0530

    Completed Array Operations

---
+++ NumPy/03_array_operations.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+
+marks = np.array([10, 20, 30, 40, 50])
+
+print(marks + 5)
+print(marks - 5)
+print(marks * 2)
+print(marks / 2)
+print("=" *70)
+
+### array  + array (Vectorization)
+
+bonus = np.array([5, 10, 15, 20, 25])
+
+print(marks + bonus)
+print(marks * bonus)
+print("=" *70)
+
